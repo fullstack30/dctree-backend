7ab297aa5ee3df2f41b1570616e9ec078875d7b6
Kelvys Moura <kelvysmoura2@gmail.com>
2024-07-20 16:49:06 -0300

    Cadastrando varios users no arquivo json

--- src/server.js
+++ src/server.js
@@ -3,15 +3,34 @@ const fs = require('node:fs');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 let html = fs.readFileSync(__dirname + '/index.html', 'utf8');
 
 app.get('/', (req, res) => {
   res.send(html);
-})
+});
 
 app.post('/produtos', (req, res) => {
-    console.log(req.params);
-    res.send('Listagem de produtos');
+    console.log(req.body);
+    res.send({});
+});
+
+
+app.post('/users', (req, res) => {
+    let data = JSON.stringify(req.body);
+    
+    let fileContent = fs.readFileSync(__dirname + '/users.json', 'utf8');
+    fileContent = JSON.parse(fileContent);
+    fileContent.push(req.body);
+
+    fs.writeFileSync(__dirname + '/users.json', JSON.stringify(fileContent));
+    
+    res.status(201);
+    res.send({
+        error: false,
+        message: 'Usuário criado com sucesso'
+    });
 })
 
 app.listen(port, () => {
